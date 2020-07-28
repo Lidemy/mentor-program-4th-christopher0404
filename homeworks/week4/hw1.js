17bf793892b383bf8eb1308eaba1805c719aad0b
christopher0404 <ctc2096@gmail.com>
2020-07-28 21:54:57 +0800

    week4 hw1

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,11 @@
+const request = require('request');
+
+const url = 'https://lidemy-book-store.herokuapp.com';
+
+request(`${url}/books?_limit=10`, (error, response, body) => {
+  if (error || response.statusCode >= 400) {
+    console.error(error);
+  }
+  const data = JSON.parse(body);
+  data.forEach(book => console.log(`${book.id} ${book.name}`));
+});
